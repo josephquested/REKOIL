f4a41f921af7345999c9fa7c09768a7081ebcffd
Joseph Quested <josephquested@gmail.com>
2017-05-25 14:45:16 +1200

    basic character controller in place

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	public void ReceiveInputDown (bool upDown, bool rightDown, bool downDown, bool leftDown)
+	{
+		if (upDown)
+		{
+			Fire(Vector2.up);
+		}
+		if (rightDown)
+		{
+			Fire(Vector2.right);
+		}
+		if (downDown)
+		{
+			Fire(Vector2.down);
+		}
+		if (leftDown)
+		{
+			Fire(Vector2.left);
+		}
+	}
+
+	public float thrust;
+
+	void Fire (Vector2 direction)
+	{
+		rb.AddForce(-direction * thrust, ForceMode2D.Impulse);
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	PlayerController controller;
+
+	void Start ()
+	{
+		controller = GetComponent<PlayerController>();
+	}
+
+	void Update ()
+	{
+		UpdateInput();
+	}
+
+	void UpdateInput ()
+	{
+		controller.ReceiveInputDown(
+			Input.GetButtonDown("Up"),
+			Input.GetButtonDown("Right"),
+			Input.GetButtonDown("Down"),
+			Input.GetButtonDown("Left")
+		);
+	}
+}
