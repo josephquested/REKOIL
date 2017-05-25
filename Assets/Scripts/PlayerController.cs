6a460718dc5e87a6ba0ee1672ac5ab1818acacf2
Joseph Quested <josephquested@gmail.com>
2017-05-25 16:25:40 +1200

    player can shoot projectiles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	// SYSTEM //
+
 	Rigidbody2D rb;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody2D>();
 	}
+
+	// INPUT //
 
 	public void ReceiveInputDown (bool upDown, bool rightDown, bool downDown, bool leftDown)
 	{
@@ -31,10 +35,16 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	// FIRE //
+
+	public GameObject projectilePrefab;
 	public float thrust;
+	public float projectileForce;
 
 	void Fire (Vector2 direction)
 	{
+		GameObject projectileObj = Instantiate(projectilePrefab, transform.position, transform.rotation);
+		projectileObj.GetComponent<Rigidbody2D>().AddForce(direction * projectileForce, ForceMode2D.Impulse);
 		rb.AddForce(-direction * thrust, ForceMode2D.Impulse);
 	}
 }
